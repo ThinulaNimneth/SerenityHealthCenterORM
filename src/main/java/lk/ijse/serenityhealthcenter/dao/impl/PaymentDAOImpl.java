6aa757d3182542de1118e679969f0cd3f079fd60
UchihaClanCoding <nimnethnetwork@gmail.com>
2026-05-20 14:00:38 +0530

    update

--- src/main/java/lk/ijse/serenityhealthcenter/dao/impl/PaymentDAOImpl.java
+++ src/main/java/lk/ijse/serenityhealthcenter/dao/impl/PaymentDAOImpl.java
@@ -1,4 +1,44 @@
 package lk.ijse.serenityhealthcenter.dao.impl;
 
-public class PaymentDAOImpl {
+import lk.ijse.serenityhealthcenter.config.FactoryConfiguration;
+import lk.ijse.serenityhealthcenter.dao.custom.PaymentDAO;
+import lk.ijse.serenityhealthcenter.entity.Payment;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class PaymentDAOImpl implements PaymentDAO {
+
+    @Override
+    public Long save(Payment payment) {
+        Session session = FactoryConfiguration.getInstance().getSession().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Long id = (Long)  session.save(payment);
+            transaction.commit();
+            return id;
+        }catch (Exception e){
+            transaction.rollback();
+            throw e;
+        }finally {
+            session.close();
+        }
+    }
+
+
+    @Override
+    public void update(Payment payment) {
+        Session session = FactoryConfiguration.getInstance().getSession().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(payment);
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+            throw e;
+        }finally {
+            session.close();
+        }
+    }
+
+
 }

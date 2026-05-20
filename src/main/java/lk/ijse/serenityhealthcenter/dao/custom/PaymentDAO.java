6e5c8ba1253a6501a08c0893f11407ab43c63da8
UchihaClanCoding <nimnethnetwork@gmail.com>
2026-05-20 13:56:04 +0530

    update

--- src/main/java/lk/ijse/serenityhealthcenter/dao/custom/PaymentDAO.java
+++ src/main/java/lk/ijse/serenityhealthcenter/dao/custom/PaymentDAO.java
@@ -1,4 +1,18 @@
 package lk.ijse.serenityhealthcenter.dao.custom;
 
+import lk.ijse.serenityhealthcenter.entity.Payment;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface PaymentDAO {
+    Long save(Payment payment);
+    void update(Payment payment);
+    void delete(Long id);
+    Optional<Payment> findById(Long id);
+    List<Payment> findAll();
+    List<Payment> findByPatient(Long patientId);
+    List<Payment> findByDateRange(LocalDate start, LocalDate end);
+    List<Payment> findByStatus(Payment.PaymentStatus status);
 }

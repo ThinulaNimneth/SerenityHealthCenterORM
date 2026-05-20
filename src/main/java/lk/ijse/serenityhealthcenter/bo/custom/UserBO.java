9433cb7ad323d8c13a5ad035c83cd17ecf9bacf2
UchihaClanCoding <nimnethnetwork@gmail.com>
2026-05-20 18:12:17 +0530

    update

--- src/main/java/lk/ijse/serenityhealthcenter/bo/custom/UserBO.java
+++ src/main/java/lk/ijse/serenityhealthcenter/bo/custom/UserBO.java
@@ -1,4 +1,16 @@
 package lk.ijse.serenityhealthcenter.bo.custom;
 
+import lk.ijse.serenityhealthcenter.dto.UserDTO;
+import lk.ijse.serenityhealthcenter.util.CustomExceptions;
+
+import java.util.List;
+
 public interface UserBO {
+    Long saveUser(UserDTO userDTO) throws CustomExceptions.RegistrationException;
+    void updateUser(UserDTO userDTO) throws CustomExceptions.RegistrationException;
+    void deleteUser(Long id);
+    UserDTO getUser(Long id);
+    List<UserDTO> getAllUsers();
+    UserDTO login(String username, String password) throws CustomExceptions.LoginException;
+    void changePassword(Long userId, String oldPassword, String newPassword) throws CustomExceptions.LoginException;
 }
--- src/main/java/lk/ijse/serenityhealthcenter/controller/LoginController.java
+++ src/main/java/lk/ijse/serenityhealthcenter/controller/LoginController.java
@@ -1,4 +1,17 @@
 package lk.ijse.serenityhealthcenter.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class LoginController {
+    @FXML private TextField txtUsername;
+    @FXML private PasswordField txtPassword;
+    @FXML private TextField txtPasswordVisible;
+    @FXML private CheckBox chkShowPassword;
+    @FXML private Button btnLogin;
+    @FXML private Label lblMessage;
+
+
+
+
 }
